52d58ec0b6e7b11c7fba5dff4784a0010ac64d19
ngoctranbao <ngoc110904@gmail.com>
2024-05-15 13:14:00 +0700

    creat new project

--- Code/app/src/main/java/ie/app/freelanchaincode/MainActivity.kt
+++ Code/app/src/main/java/ie/app/freelanchaincode/MainActivity.kt
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import ie.app.freelanchaincode.auth.EditProfileActivity
+import ie.app.freelanchaincode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button:Button
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button = findViewById(R.id.editButton)
-        button.setOnClickListener {
+        binding.editButton.setOnClickListener {
             Intent(
                 this@MainActivity,
                 EditProfileActivity::class.java
+            ).also { startActivity(it) }
+        }
+        binding.postProjectButton.setOnClickListener {
+            Intent(
+                this@MainActivity,
+                PostProjectActivity::class.java
             ).also { startActivity(it) }
         }
     }
